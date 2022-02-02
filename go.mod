c46262636d8d5af1e6d6670d41e549e4db136173
michalswi <m.swierczewski1987@gmail.com>
2022-02-02 17:09:57 +0100

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/michalswi/geolocator
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var logger = log.New(os.Stdout, "web-server ", log.LstdFlags|log.Lshortfile|log.Ltime|log.LUTC)
+var port = getEnv("SERVER_PORT", "8080")
+
+func main() {
+	http.HandleFunc("/", readme)
+	http.HandleFunc("/geo", getResponse)
+	logger.Println("Server is ready to handle requests at port", port)
+	logger.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func readme(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func getResponse(w http.ResponseWriter, r *http.Request) {
+
+	var request []string
+
+	// Request line
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+
+	// Header
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	for name, headers := range r.Header {
+		// name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	fmt.Println(strings.Join(request, "\n"))
+
+	// Body
+	if r.Method == "POST" {
+		body, _ := ioutil.ReadAll(r.Body)
+		fmt.Println(string(body))
+	}
+
+	fmt.Println()
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return value
+}
